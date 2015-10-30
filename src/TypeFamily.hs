320cc5951452a2b3118ce0a2a10cbad648169c82
Greg Wiley <gwiley@cj.com>
2015-10-30 13:32:07 -0700

    battle demo

--- src/TypeFamily.hs
+++ src/TypeFamily.hs
@@ -19,3 +19,46 @@ pickWaterMove _ = WaterGun
 
 pickGrassMove :: Grass -> GrassMove
 pickGrassMove _ = VineWhip
+
+printBattle :: String -> String -> String -> String -> String -> IO ()
+printBattle pk1 mv1 pk2 mv2 winner = do
+  putStrLn $ pk1 ++ " used " ++ mv1
+  putStrLn $ pk2 ++ " used " ++ mv2
+  putStrLn $ "Winner is: " ++ winner
+  putStrLn ""
+
+battleWaterVsFire :: Water -> Fire -> IO ()
+battleWaterVsFire water fire = do
+  printBattle (show water) (show wmove) (show fire) (show fmove) (show water)
+  where
+    wmove = pickWaterMove water
+    fmove = pickFireMove fire
+
+battleFireVsWater = flip battleWaterVsFire
+
+battleGrassVsWater :: Grass -> Water -> IO ()
+battleGrassVsWater grass water = do
+  printBattle (show grass) (show gmove) (show water) (show wmove) (show grass)
+  where
+    gmove = pickGrassMove grass
+    wmove = pickWaterMove water
+
+battleWaterVsGrass = flip battleGrassVsWater
+
+battleFireVsGrass :: Fire -> Grass -> IO ()
+battleFireVsGrass fire grass = do
+  printBattle (show fire) (show fmove) (show grass) (show gmove) (show fire)
+  where
+    fmove = pickFireMove fire
+    gmove = pickGrassMove grass
+
+battleGrassVsFire = flip battleFireVsGrass
+
+battleEx1 :: IO ()
+battleEx1 = do
+  battleWaterVsFire Squirtle Charmander
+  battleFireVsWater Charmeleon Wartortle
+  battleGrassVsWater Bulbasaur Blastoise
+  battleWaterVsGrass Wartortle Ivysaur
+  battleFireVsGrass Charmeleon Ivysaur
+  battleGrassVsFire Venusaur Charizard
