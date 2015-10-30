0c2b17fb150b6bf82148ab8143cdfcac69fcdb53
Greg Wiley <gwiley@cj.com>
2015-10-30 09:11:49 -0700

    simple model

--- src/TypeFamily.hs
+++ src/TypeFamily.hs
@@ -1,2 +1,21 @@
 module TypeFamily where
 
+data Fire = Charmander | Charmeleon | Charizard deriving Show
+data Water = Squirtle | Wartortle | Blastoise deriving Show
+data Grass = Bulbasaur | Ivysaur | Venusaur deriving Show
+
+data FireMove = Ember | FlameThrower | FireBlast deriving Show
+data WaterMove = Bubble | WaterGun deriving Show
+data GrassMove = VineWhip deriving Show
+
+pickFireMove :: Fire -> FireMove
+pickFireMove Charmander = Ember
+pickFireMove Charmeleon = FlameThrower
+pickFireMove Charizard = FireBlast
+
+pickWaterMove :: Water -> WaterMove
+pickWaterMove Squirtle = Bubble
+pickWaterMove _ = WaterGun
+
+pickGrassMove :: Grass -> GrassMove
+pickGrassMove _ = VineWhip
